f8fced5ee0c5173ef94a89cd9d67b3ede945f83e
Patrick Siqueira <psique.estuda@gmail.com>
2023-02-23 17:25:10 -0300

    link between Angular build and Laravel view

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Album de Fotos</title>
+    </head>
+    <body>
+    <app-root></app-root>
+    <script src="{{asset('js/runtime.4a8f2ce34fb98ca5.js')}}" type="module"></script>
+    <script src="{{asset('js/polyfills.149b7fcd68f5bc0e.js')}}" type="module"></script>
+    <script src="{{asset('js/main.e7b6944a49532a20.js')}}" type="module"></script>
+    </body>
+</html>
