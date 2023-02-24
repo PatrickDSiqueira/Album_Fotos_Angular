6103460653115e781dc4fe0f11e8455b36eb932b
Patrick Siqueira <psique.estuda@gmail.com>
2023-02-24 15:01:01 -0300

    Create controller and migrate

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
+    public function like($id)
+    {
+        //
+    }
+}
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-            <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-        <title>Album de Fotos</title>
-    </head>
-    <body>
-    <app-root></app-root>
-    <script src="{{asset('js/runtime.4a8f2ce34fb98ca5.js')}}" type="text/javascript"></script>
-    <script src="{{asset('js/polyfills.149b7fcd68f5bc0e.js')}}" type="text/javascript"></script>
-    <script src="{{asset('js/main.3af112f411d9a564.js')}}" type="text/javascript"></script>
-    </body>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <title>Album de Fotos</title>
+    <link rel="stylesheet" href="{{asset('styles.9fe34a53a36a4e6c.css')}}" media="print" onload="this.media='all'">
+
+</head>
+<body>
+<app-root></app-root>
+<script src="{{asset('js/runtime.4a8f2ce34fb98ca5.js')}}" type="module"></script>
+<script src="{{asset('js/polyfills.149b7fcd68f5bc0e.js')}}" type="module"></script>
+<script src="{{asset('js/main.7ea0afbceb0d58b8.js')}}" type="module"></script>
+</body>
 </html>
