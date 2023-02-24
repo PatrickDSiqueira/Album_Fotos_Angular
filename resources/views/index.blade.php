9cdb5d973412b8ed2d4a07462c5d2dfa7d66ac35
Patrick Siqueira <psique.estuda@gmail.com>
2023-02-24 09:32:58 -0300

    Create a main page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,13 +3,13 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+            <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
         <title>Album de Fotos</title>
     </head>
     <body>
     <app-root></app-root>
-    <script src="{{asset('js/runtime.4a8f2ce34fb98ca5.js')}}" type="module"></script>
-    <script src="{{asset('js/polyfills.149b7fcd68f5bc0e.js')}}" type="module"></script>
-    <script src="{{asset('js/main.e7b6944a49532a20.js')}}" type="module"></script>
+    <script src="{{asset('js/runtime.4a8f2ce34fb98ca5.js')}}" type="text/javascript"></script>
+    <script src="{{asset('js/polyfills.149b7fcd68f5bc0e.js')}}" type="text/javascript"></script>
+    <script src="{{asset('js/main.3af112f411d9a564.js')}}" type="text/javascript"></script>
     </body>
 </html>
