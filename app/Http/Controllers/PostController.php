3689c3e6ac1d7469e1e530d99577936978821473
Patrick Siqueira <psique.estuda@gmail.com>
2023-02-24 15:16:40 -0300

    Implementando a API

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,26 +2,53 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class PostController extends Controller
 {
     public function index()
     {
-        //
+        return Post::all();
     }
 
     public function store(Request $request)
     {
-        //
+        $path = $request -> file('file')->store('image','public');
+        $post = new Post();
+        $post -> name = $request->name;
+        $post -> email= $request->email;
+        $post -> title = $request->title;
+        $post -> subtitle= $request->subtitle;
+        $post -> message= $request->message;
+        $post -> file = $request->$path;
+        $post -> likes = 0;
+
+        $post -> save();
     }
 
     public function destroy($id)
     {
-        //
+        $post = Post::find($id);
+
+        if (isset($post)){
+            Storage::disk('public')->delete($post->file);
+            $post ->delete();
+            return 204;
+        }
+        return reponse('Post não encontrado',404);
     }
+
     public function like($id)
     {
-        //
+        $post = Post::find($id);
+
+        if (isset($post)){
+            $post ->likes++;
+            $post->save();
+            return $post;
+        }
+        return  response("Id não encontrado", 404);
     }
 }
